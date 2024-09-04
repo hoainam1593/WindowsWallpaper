a1fc7eecd73b78f68624b049068ad69f19f4a58e
𝔑𝔞𝔪 𝔑𝔤𝔲𝔶𝔢𝔫 <nguyenhoainam@hoppergames.io>
2024-09-04 17:08:43 +0700

    get image unsplash

--- ImageProviders/ImageProvider_Unsplash.cs
+++ ImageProviders/ImageProvider_Unsplash.cs
@@ -1,7 +1,19 @@
-﻿namespace WindowsWallpaper.ImageProviders
+﻿using Newtonsoft.Json;
+
+namespace WindowsWallpaper.ImageProviders
 {
     internal class ImageProvider_Unsplash : IImageProvider
     {
+        internal class Jobject
+        {
+            internal class Url
+            {
+                public string full;
+            }
+
+            public Url urls;
+        }
+
         const string baseUrl = "https://api.unsplash.com";
         const string action = "photos/random";
         static Dictionary<string, string> param = new Dictionary<string, string>()
@@ -16,7 +28,18 @@
         {
             var urlBuilder = new UrlBuilder(baseUrl, action, param);
             var json = await Downloader.DownloadText(urlBuilder.Url);
-            return new List<string>() { json };
+            return ParseJson(json);
+        }
+
+        public List<string> ParseJson(string json)
+        {
+            var lJobject = JsonConvert.DeserializeObject<List<Jobject>>(json);
+            var result = new List<string>();
+            foreach (var i in lJobject)
+            {
+                result.Add(i.urls.full);
+            }
+            return result;
         }
     }
 }
